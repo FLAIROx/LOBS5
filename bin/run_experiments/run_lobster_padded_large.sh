f79a94787731b69721ac3436e1071d8c0eb84d55
isambard <kang@robots.ox.ac.uk>
2025-12-08 14:21:44 +0000

    lr bsz

--- bin/run_experiments/run_lobster_padded_large.sh
+++ bin/run_experiments/run_lobster_padded_large.sh
@@ -127,11 +127,14 @@ python -u -B run_train.py \
         --model_preset=$MODEL_PRESET \
         --C_init=trunc_standard_normal --prenorm=True --batchnorm=False --bidirectional=False \
         --dataset=lobster-prediction --merging=padded \
+        --dir_name='/lus/lfs1aip2/home/s5e/kangli.s5e/GOOG_GOOGL_2016TO2021_24tok_encoded/GOOG/2021' \
+        --test_dir_name='/lus/lfs1aip2/home/s5e/kangli.s5e/JAN2023/GOOG_24tok_encoded' \
+        --data_mode='encoded' \
         --dir_name='/lus/lfs1aip2/home/s5e/kangli.s5e/GOOG_GOOGL_2016TO2021_24tok_preproc/GOOG/2021' \
         --test_dir_name='/lus/lfs1aip2/home/s5e/kangli.s5e/JAN2023/GOOG_24tok_preproc' \
         --data_mode='preproc' \
         --clip_eigs=True --activation_fn=half_glu1 \
-        --dt_global=False --epochs=20 --jax_seed=42 \
+        --dt_global=False --epochs=100 --jax_seed=42 \
         --opt_config=standard --p_dropout=0.0 \
         --warmup_end=1 --weight_decay=0.05 --msg_seq_len=500 \
         --use_book_data=True --use_simple_book=False --book_transform=True  \
@@ -146,6 +149,15 @@ python -u -B run_train.py \
         --USE_WANDB=True \
         --wandb_entity=kang-oxford
 
+
+        # --dir_name='/lus/lfs1aip2/home/s5e/kangli.s5e/GOOG_GOOGL_2016TO2021_24tok_encoded/GOOG/2021' \
+        # --test_dir_name='/lus/lfs1aip2/home/s5e/kangli.s5e/JAN2023/GOOG_24tok_encoded' \
+        # --data_mode='encoded' \
+
+
+        # --dir_name='/lus/lfs1aip2/home/s5e/kangli.s5e/GOOG_GOOGL_2016TO2021_24tok_preproc/GOOG/2021' \
+        # --test_dir_name='/lus/lfs1aip2/home/s5e/kangli.s5e/JAN2023/GOOG_24tok_preproc' \
+        # --data_mode='preproc' \
 
         # --dir_name='/lus/lfs1aip2/home/s5e/kangli.s5e/GOOG_GOOGL_2016TO2021_24tok_encoded/GOOG' \
         # --test_dir_name='/lus/lfs1aip2/home/s5e/kangli.s5e/JAN2023/GOOG_24tok_encoded' \
