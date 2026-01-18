e2d7227d2fab5eca92dadd76d7a662e1e0fc84bd
isambard <kang@robots.ox.ac.uk>
2026-01-18 14:29:54 +0000

    docs: add agent_trades definition to training log

--- es_lobs5/training/es_trainer.py
+++ es_lobs5/training/es_trainer.py
@@ -2443,6 +2443,7 @@ class ESTrainer:
         print(" [Market Orders] : liquidation_quantity (Executed by force_market_order at end)")
         print(" [Doom Orders]   : doom_quantity        (Unfilled quantity, subject to penalty)")
         print(" [Fill Rates]    : quantity / task_size")
+        print(" [Agent Trades]  : agent_trades         (Count of distinct trade executions/fills)")
         print("="*60)
 
         # Resume from checkpoint if specified
