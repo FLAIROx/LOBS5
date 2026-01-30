4c47ed9fb66ab465adb461962a868e50c77ee3d8
isambard <kang@robots.ox.ac.uk>
2026-01-30 22:03:02 +0000

    update run duration

--- es_lobs5/scripts/es_training.sh
+++ es_lobs5/scripts/es_training.sh
@@ -5,7 +5,7 @@
 #SBATCH --gpus-per-node=4
 #SBATCH --gres=gpu:4
 #SBATCH --mem=0
-#SBATCH --time=24:00:00
+#SBATCH --time=01:00:00
 #SBATCH --output=logs/es_train_%j.out
 #SBATCH --error=logs/es_train_%j.err
 #SBATCH --partition=workq
